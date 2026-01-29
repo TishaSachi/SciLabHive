88970f83e8269abdec3b0b9b60d4a9bcacff2219
TishaSachi <tishagamalath@gmail.com>
2026-01-29 21:54:34 +0530

    Updated security.py

--- scilabhive-backend/security.py
+++ scilabhive-backend/security.py
@@ -1,11 +1,15 @@
-from datetime import datetime, timedelta
-from jose import jwt
-from passlib.context import CryptContext
+from sqlalchemy.orm import Session
 from fastapi import Depends, HTTPException, status
+from datetime import datetime, timedelta
+from passlib.context import CryptContext
+
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 import os
 from dotenv import load_dotenv
+
+from database import get_db
+from models import User
 
 load_dotenv()
 
@@ -32,15 +36,29 @@ def create_access_token(data: dict):
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
-def get_current_user(token: str = Depends(oauth2_scheme)):
+def get_current_user(
+        token: str = Depends(oauth2_scheme),
+        db: Session = Depends(get_db)
+):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         user_id: str = payload.get("sub")
 
         if user_id is None:
-            raise HTTPException(status_code=401, detail="Invalid token")
+            raise HTTPException(
+                status_code=401, 
+                detail="Invalid token"
+            )
+        
+        user = db.query(User).filter(User.id == int(user_id)).first()
 
-        return int(user_id)
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="User not found"
+            )
+
+        return user   # return full user object
 
     except JWTError:
         raise HTTPException(
