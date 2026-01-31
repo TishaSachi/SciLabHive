f31bdeb49480fac31c517ae966667d5132a51573
TishaSachi <tishagamalath@gmail.com>
2026-01-31 14:07:10 +0530

    Completed CRUD operations for Experiment Results

--- scilabhive-backend/routes/experiment_results.py
+++ scilabhive-backend/routes/experiment_results.py
@@ -12,7 +12,7 @@ from security import get_current_user
 
 
 router = APIRouter(
-    prefix="/experiments",
+    prefix="/experiment_results",
     tags=["ExperimentResults"]
 )
 
@@ -72,3 +72,68 @@ def get_experiment_results(
     return db.query(ExperimentResult).filter(
         ExperimentResult.experiment_id == experiment_id
     ).all()
+
+
+# --------------- Update Experiment Results ------------------
+    
+@router.put("/{result_id}", response_model=ExperimentResultResponse)
+def update_experiment_result(
+    result_id: int,
+    result_data: ExperimentResultCreate,
+    db: Session = Depends(get_db),
+    current_user = Depends(get_current_user)
+):
+    # Get parameter
+    result = db.query(ExperimentResult).filter(
+        ExperimentResult.result_id == result_id
+    ).first()
+
+    if not result:
+        raise HTTPException(status_code=404, detail="Parameter not found")
+
+    # Ownership check via experiment
+    experiment = db.query(Experiment).filter(
+        Experiment.experiment_id == result.experiment_id,
+        Experiment.user_id == current_user.id
+    ).first()
+
+    if not experiment:
+        raise HTTPException(status_code=403, detail="Not authorized")
+
+    # Update fields
+    result.result_name = result_data.result_name
+    result.result_value = result_data.result_value
+
+    db.commit()
+    db.refresh(result)
+
+    return result
+
+# ---------------------------- Delete Experiment Parameters -------------------------
+
+@router.delete("/{result_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_experiment_result(
+    result_id: int,
+    db: Session = Depends(get_db),
+    current_user: int = Depends(get_current_user)
+):
+    result = db.query(ExperimentResult).filter(
+        ExperimentResult.result_id == result_id
+    ).first()
+
+    if not result:
+        raise HTTPException(status_code=404, detail="Parameter not found")
+
+    # Ownership check
+    experiment = db.query(Experiment).filter(
+        Experiment.experiment_id == result.experiment_id,
+        Experiment.user_id == current_user.id
+    ).first()
+
+    if not experiment:
+        raise HTTPException(status_code=403, detail="Not authorized")
+
+    db.delete(result)
+    db.commit()
+
+    return
